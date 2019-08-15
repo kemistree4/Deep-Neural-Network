55991534706572444ef606fc07f87186f3636202
kemistree4 <46361264+kemistree4@users.noreply.github.com>
2019-08-14 17:58:03 -0700

    Create DNN_test.py

---
+++ DNN_test.py
@@ -0,0 +1,61 @@
+import numpy as np
+import mnist
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+
+train_images = mnist.train_images()
+train_labels = mnist.train_labels()
+test_images = mnist.test_images()
+test_labels = mnist.test_labels()
+
+# Normalize the images.
+train_images = (train_images / 255) - 0.5
+test_images = (test_images / 255) - 0.5
+
+# Flatten the images.
+train_images = train_images.reshape((-1, 784))
+test_images = test_images.reshape((-1, 784))
+
+model = Sequential([
+  Dense(64, activation='relu', input_shape=(784,)),
+  Dense(64, activation='relu'),
+  Dense(10, activation='softmax'),
+])
+
+# Compile the model.
+model.compile(
+  optimizer='adam',
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+# Train the model.
+model.fit(
+  train_images,
+  to_categorical(train_labels),
+  epochs=10,
+  batch_size=32,
+)
+
+# Evaluate the model.
+model.evaluate(
+  test_images,
+  to_categorical(test_labels)
+)
+
+# Save the model to disk.
+model.save_weights('model.h5')
+
+# Load the model from disk later using:
+# model.load_weights('model.h5')
+
+# Predict on the first 5 test images.
+predictions = model.predict(test_images[:5])
+
+# Print model's predictions.
+print(np.argmax(predictions, axis=1)) 
+
+# Check predictions against the ground truths.
+print(test_labels[:5]) 
